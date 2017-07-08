4d980932a7359f93a98b5b7f60827459decb4cb5
bystrov-W <bystrov.v.v@outlook.com>
2017-07-08 23:38:02 +0300

    Delete admin.php

--- admin.php
+++
@@ -1,5 +0,0 @@
-<form enctype="multipart/form-data" action="list.php" method="post">
-<input type="hidden" name="MAX_FILE_SIZE" value="30000">
-Отправить: <input name="userfile" type="file">
-<input type="submit" value="Send File">
-</form>
